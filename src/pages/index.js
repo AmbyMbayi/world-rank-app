2cfc77582b6bf1359c581d58e971b0be94966190
Amby Mbayi <am@workpay.co.ke>
2021-07-02 13:38:20 +0300

    filtering using region subregion country

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,28 @@ import Layout from '../components/Layout/Layout';
 import styles from '../styles/Home.module.css';
 import SearchInput from '../components/SearchInput/SearchInput';
 import CountriesTable from '../components/CountriesTable/CountriesTable';
+import { useState } from 'react';
 
 export default function Home({ countries }) {
 	console.log(countries);
+	const [ keyword, setKeyword ] = useState('');
+
+	const filterCountries = countries.filter(
+		(country) =>
+			country.name.toLowerCase().includes(keyword) ||
+			country.region.toLowerCase().includes(keyword) ||
+			country.subregion.toLowerCase().includes(keyword)
+	);
+
+	const onInputChange = (e) => {
+		e.preventDefault();
+		setKeyword(e.target.value.toLowerCase());
+	};
 	return (
 		<Layout>
 			<div className={styles.counts}>Found {countries.length} countries</div>
-			<SearchInput placeholder="filter by Name, Region or SubRegion" />
-			<CountriesTable countries={countries} />
+			<SearchInput placeholder="filter by Name, Region or SubRegion" onChange={onInputChange} />
+			<CountriesTable countries={filterCountries} />
 		</Layout>
 	);
 }
